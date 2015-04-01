89c414c7325ce1eee65be3f3c8edd9ffc0507e91
Osthir Nobin <Osthir Nobin@Sima-Niaz.home>
2015-03-31 20:45:53 -0700

    square display done

--- SquareDisplay/src/squaredisplay/SquareDisplay.java
+++ SquareDisplay/src/squaredisplay/SquareDisplay.java
@@ -1,47 +1,44 @@
-
-package squaredisplay;
+package squareDisplay;
 
 import javax.swing.JOptionPane;
 
 /**
  *
- * @author Osthir Nobin
+ * @author Niaz Morshed
  */
 public class SquareDisplay {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        String userInput;
-        String output =" ";
-        int number ;
-        int counterOne;
-        int counterTwo;
-        
-        
-        userInput =JOptionPane.showInputDialog("Please enter a value 1-15: ");
-            
-       number = Integer.parseInt(userInput);
-        
-        
-        if (number >=1 && number <=15){
-            
-            for(counterOne = 0; counterOne < number; counterOne++){
-                
-                for(counterTwo = 0; counterTwo < number; counterTwo++){
-                    System.out.print("X");
-                }
-                
-                System.out.print("\n");
+    public static void main(String args[]) {
+        String userInput; // To hold the userInput values 
+        String output = ""; // To hold the string for output to the user
+        int number = 0;
+        int counterOne; // To hold the value for outside loop 
+        int counterTwo; // To hold the value for inside loop 
+
+        // Check to make sure input fits the guide lines; must be 1-15 
+        while (number < 1 || number > 15) {
+            // Get input from the user 
+            userInput = JOptionPane.showInputDialog("Please enter a value 1-15: ");
+
+            // Convert user input in to an integer 
+            number = Integer.parseInt(userInput);
+
+        } // End while loop for guide line checker 
+
+        // Run a two For loops to create the square 
+        // Outside loop adds another row 
+        for (counterOne = 0; counterOne < number; counterOne++) {
+
+            // Inner loop places an appropriate number of X's in a row 
+            for (counterTwo = 0; counterTwo < number; counterTwo++) {
+                output = output + 'X';
             }
-            System.exit(0);
+            // Go to next line 
+            output = output + '\n';
         }
-        
-        else{
-            JOptionPane.showMessageDialog(null, "Invalid number");
-        }
+
+        JOptionPane.showMessageDialog(null, output);
+
+        System.exit(0);
     }
-    
-    
 }
