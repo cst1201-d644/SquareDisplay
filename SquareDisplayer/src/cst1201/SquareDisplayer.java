806b2b7635d9fe2a5d74eb49a9a4ae464e16f9c9
15320635 <15320635@CST_N417_07.cst.citytech.cuny.edu>
2015-04-01 16:56:27 -0400

    Made some changes

--- SquareDisplayer/src/cst1201/SquareDisplayer.java
+++ SquareDisplayer/src/cst1201/SquareDisplayer.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package cst1201;
 
 import java.util.Scanner;
@@ -18,21 +17,25 @@ public class SquareDisplayer {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+
         System.out.print("Enter a positive integer <= 15: ");
         Scanner scan = new Scanner(System.in);
         int input = scan.nextInt();
-        
-        while (input < 1 || input > 15){
+
+        while (input < 1 || input > 15) {
             System.out.print("Incorrect. Please enter another number: ");
             input = scan.nextInt();
         }
-        
-        for (int i = 0; i < input; i++){
+
+        for (int i = 0; i < input; i++) {
             System.out.print("X");
-            for (int j 
+            for (int j = 1; j < input; j++) 
+                System.out.print("X");
+            
+            System.out.println();
         }
+
         System.out.println();
     }
-    
+
 }
