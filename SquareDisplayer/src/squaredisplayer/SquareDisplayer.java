4448219de6114e22fcbb93df9c8499448f1059de
Jean-Luc <Jean-Luc@Jean-Luc-PC.home>
2015-04-01 23:49:04 -0400

    Final SquareDisplayer

--- SquareDisplayer/src/squaredisplayer/SquareDisplayer.java
+++ SquareDisplayer/src/squaredisplayer/SquareDisplayer.java
@@ -3,8 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package squaredisplayer;
+
+import java.util.Scanner;
 
 /**
  *
@@ -16,7 +17,28 @@ public class SquareDisplayer {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //1. Get input from user.
+        System.out.print("Please enter a positive integer less than or equal to 15: ");
+        Scanner scanner = new Scanner(System.in);
+        int inputNumber = scanner.nextInt();
+
+        while (inputNumber < 1 || inputNumber > 15) {
+        System.out.print("Incorrect. Please enter another number: ");
+        inputNumber = scanner.nextInt();
+
+        }
+       
+        for (int i = 0; i < inputNumber; i++) 
+        {
+            for (int z = 0; z < inputNumber; z++) {
+                System.out.print("X");
+            }
+
+            System.out.println();
+            
+        }
+
+        }
+
     }
-    
-}
+
